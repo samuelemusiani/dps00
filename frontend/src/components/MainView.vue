bebed4b9b855142c8e2ca9d8876ab5727a6ba17a
Samuele Musiani <samu@teapot.ovh>
2026-06-15 17:12:39 +0200

    add: daisyui

--- frontend/src/components/MainView.vue
+++ frontend/src/components/MainView.vue
@@ -1,9 +1,23 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>
-    <h1 class="font-bold">Lagovia Train Tracker</h1>
-    <p>Body</p>
+  <div class="flex flex-col items-center gap-4">
+    <h1 class="font-bold text-5xl">Lagovia Train Tracker</h1>
+    <label class="input">
+      <svg class="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+        <g
+          stroke-linejoin="round"
+          stroke-linecap="round"
+          stroke-width="2.5"
+          fill="none"
+          stroke="currentColor"
+        >
+          <circle cx="11" cy="11" r="8"></circle>
+          <path d="m21 21-4.3-4.3"></path>
+        </g>
+      </svg>
+      <input type="search" required placeholder="Search" />
+    </label>
   </div>
 </template>
 
