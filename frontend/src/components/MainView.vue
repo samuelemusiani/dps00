4e27ebc36aaab70dc303c622c23308526ffa5934
Samuele Musiani <samu@teapot.ovh>
2026-06-15 17:14:59 +0200

    add: iconify

--- frontend/src/components/MainView.vue
+++ frontend/src/components/MainView.vue
@@ -1,21 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Icon } from '@iconify/vue'
+</script>
 
 <template>
   <div class="flex flex-col items-center gap-4">
     <h1 class="font-bold text-5xl">Lagovia Train Tracker</h1>
     <label class="input">
-      <svg class="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-        <g
-          stroke-linejoin="round"
-          stroke-linecap="round"
-          stroke-width="2.5"
-          fill="none"
-          stroke="currentColor"
-        >
-          <circle cx="11" cy="11" r="8"></circle>
-          <path d="m21 21-4.3-4.3"></path>
-        </g>
-      </svg>
+      <Icon icon="mdi:magnify" class="text-gray-500" />
       <input type="search" required placeholder="Search" />
     </label>
   </div>
